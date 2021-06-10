f95a5a707260cf2b3d3a0e5ff8b6294d35453202
SDW9999 <onpyy29@gmail.com>
2021-06-10 13:37:06 +0900

    14681

---
+++ 2.if문/[14681]사분면고르기/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+
+	if (x > 0 && y > 0) // +x +y
+		cout << "1" << endl;
+	else if (x < 0 && y > 0) // -x +y
+		cout << "2" << endl;
+	else if (x < 0 && y < 0) // -x -y
+		cout << "3" << endl;
+	else 
+		cout << "4" << endl; // +x -y
+
+	return 0;
+}
